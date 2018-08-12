8b17d9fe25b7c62752a68bb972295ed87d80bf2f
Hugo Xu <jspenguin2017@users.noreply.github.com>
2018-08-12 09:57:25 -0600

    Update jit_calc.cpp

--- jit_calc.cpp
+++ jit_calc.cpp
@@ -60,26 +60,26 @@ bool fetchInt(std::string& str, size_t& strLen, size_t& iterator, int& out)
 
 int main()
 {
-	//Startup
+	// Startup
 	SetConsoleTitle(TEXT("JIT Compiling Calculator"));
 	std::cout << "This is probably the most useless tool ever" << std::endl;
 	std::cout << "It can only do 1 to 4 digits multiplication, enter something like \"125 * 20\" and see if it works" << std::endl;
 
-	//Get user input
+	// Get user input
 	std::string str;
 	std::getline(std::cin, str);
 
-	//Prepare variables
+	// Prepare variables
 	size_t iterator = 0;
 	size_t strLen = str.length();
 	int op1, op2;
 	int JITReturn;
 
-	//Prepare JIT payload buffer
-	//http://ref.x86asm.net/coder32.html
+	// Prepare JIT payload buffer
+	// http://ref.x86asm.net/coder32.html
 	byte* payload = (byte*)VirtualAllocEx(GetCurrentProcess(), 0, 512, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 
-	//Check if memory allocation succeeded
+	// Check if memory allocation succeeded
 	if (payload == NULL)
 	{
 		//Failed, can't even get 512 bytes of memory...
@@ -91,12 +91,12 @@ int main()
 	{
 		byte* p = payload;
 
-		//Prepare
-		*p++ = 0x50; //Push EAX
-		*p++ = 0x52; //Push EDX
+		// Prepare
+		*p++ = 0x50; // Push EAX
+		*p++ = 0x52; // Push EDX
 
-		//Put in first operand
-		*p++ = 0xA1; //Mov EAX, (operand1)
+		// Put in first operand
+		*p++ = 0xA1; // Mov EAX, (operand1)
 		if (!fetchInt(str, strLen, iterator, op1))
 		{
 			pause();
@@ -104,25 +104,25 @@ int main()
 		}
 		(int*&)p[0] = &op1; p += sizeof(int*);
 
-		//Move it over
-		*p++ = 0x92; //Xchg EDX, EAX
+		// Move it over
+		*p++ = 0x92; // Xchg EDX, EAX
 
-		//Get operator
+		// Get operator
 		while (str[iterator] == ' ' && iterator < strLen)
 		{
 			iterator++;
 		}
 		if (iterator >= strLen || str[iterator] != '*')
 		{
-			//Only multiplication is supported
+			// Only multiplication is supported
 			std::cout << "Unexpected character or end of input at position " << iterator << std::endl;
 			pause();
 			return 1;
 		}
 		iterator++;
 
-		//Put in second operand
-		*p++ = 0xA1; //Mov EAX, (operand2)
+		// Put in second operand
+		*p++ = 0xA1; // Mov EAX, (operand2)
 		if (!fetchInt(str, strLen, iterator, op2))
 		{
 			pause();
@@ -130,24 +130,24 @@ int main()
 		}
 		(int*&)p[0] = &op2; p += sizeof(int*);
 
-		//Do multiplication
+		// Do multiplication
 		*p++ = 0xF7; *p++ = 0xEA; //Imul EDX
 
-		//Return result
-		*p++ = 0xA3; //Mov (JITReturn), EAX
+		// Return result
+		*p++ = 0xA3; // Mov (JITReturn), EAX
 		(int*&)p[0] = &JITReturn; p += sizeof(int*);
 
-		//Clean up
+		// Clean up
 		*p++ = 0x5A; //Pop EDX
 		*p++ = 0x58; //Pop EAX
 		*p++ = 0xC3; //Ret
 
-		//Execute payload
+		// Execute payload
 		Function f;
 		f.payload = payload;
 		f.execute();
 
-		//Print result
+		// Print result
 		std::cout << JITReturn << std::endl;
 		pause();
 		return 0;
